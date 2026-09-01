ad66cacf7bf07b3c5630412bb91ec1b665ddf4db
nakanishy <nakanishy@yahoo.com>
2026-09-01 10:44:16 +0900

    add icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,14 @@ export function Header(props: PropsWithClassName) {
   return (
     <Centering className={props.className}>
       <header className="flex items-center justify-between px-8 h-[70px]">
-        <h1 className="text-5 font-bold text-white/90">Unstash</h1>
+        <div className="flex items-center gap-4">
+          <img
+            className="block size-[24px]"
+            src="/images/unstash.svg"
+            alt="Unstash"
+          />
+          <h1 className="text-5 font-bold text-white/90">Unstash</h1>
+        </div>
       </header>
     </Centering>
   );
